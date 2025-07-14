4a958ffe549dc8c045957048e01a594dceb08a32
hms1218 <rhkwmq93@naver.com>
2025-07-14 15:39:00 +0900

    first-commit

--- BackEnd/project/src/main/java/com/project/project/entity/UserEntity.java
+++ BackEnd/project/src/main/java/com/project/project/entity/UserEntity.java
@@ -1,5 +1,52 @@
 package com.project.project.entity;
 
-public class UserEntity {
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="users")
+public class UserEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userNo; //pk
+	
+	@Column(nullable = false, unique = true, length = 15)
+	private String userId;
+	
+	@Column(nullable = false)
+	private String password;
+	
+	@Column(nullable = false, unique = true, length = 100)
+	private String email;
+	
+	private LocalDateTime createdAt;
+	
+	private LocalDateTime updatedAt;
+	
+	@PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
